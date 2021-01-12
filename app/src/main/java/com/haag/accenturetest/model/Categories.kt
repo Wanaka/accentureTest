9f1b35d1e9ff7473ae5e36cb4dcdd3647651b342
Wanaka <jonas.haag@hotmail.com>
2021-01-12 12:42:48 +0100

    implemented room database and logic of making api call only the first time and then call from room db

--- app/src/main/java/com/haag/accenturetest/model/Categories.kt
+++ app/src/main/java/com/haag/accenturetest/model/Categories.kt
@@ -1,11 +1,18 @@
 package com.haag.accenturetest.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@Entity(tableName = "categories_table")
 data class Categories(
+    @PrimaryKey(autoGenerate = false)
+    @ColumnInfo(name = "category")
     @SerializedName("category_name") var categories: String,
+    @ColumnInfo(name = "type")
     @SerializedName("type") var type: Int
-): Parcelable
+) : Parcelable
--- app/src/main/java/com/haag/accenturetest/ui/categories/CategoriesViewModel.kt
+++ app/src/main/java/com/haag/accenturetest/ui/categories/CategoriesViewModel.kt
@@ -3,41 +3,55 @@ package com.haag.accenturetest.ui.categories
 import android.util.Log.d
 import android.view.View
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.navigation.findNavController
 import com.haag.accenturetest.model.Categories
 import com.haag.accenturetest.repository.CategoriesRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class CategoriesViewModel @ViewModelInject constructor(private val categoriesRepo: CategoriesRepository) :
     ViewModel() {
 
     var TAG = "CategoriesViewModel"
 
-    fun getCategoriesApi(): LiveData<List<Categories>> {
+    fun getCategoriesDB(): LiveData<List<Categories>> {
         var categories = MutableLiveData<List<Categories>>()
 
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 val deferred =
-                    async { categoriesRepo.getCategoriesApi().sortedBy { it.categories } }
+                    async { categoriesRepo.getCategories().sortedBy { it.categories } }
 
                 withContext(Dispatchers.Main) {
                     categories.value = deferred.await()
-                }
 
+                    if (categories.value.isNullOrEmpty()) {
+                        val def =
+                            async { categoriesRepo.getCategoriesApi().sortedBy { it.categories } }
+                        categories.value = def.await()
+                        updateRoomDB(def.await())
+                    }
+                }
             } catch (e: Exception) {
                 d(TAG, "Exception: ${e.message}")
             }
         }
 
         return categories
+    }
+
+    private fun updateRoomDB(list: List<Categories>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                withContext(Dispatchers.Default) {
+                    list
+                }.forEach {
+                    launch { categoriesRepo.saveCategories(it) }
+                }
+            } catch (e: Exception) {
+                d(TAG, "Exception: ${e.message}")
+            }
+        }
     }
 
     fun navigateTo(view: View, category: Categories) {
