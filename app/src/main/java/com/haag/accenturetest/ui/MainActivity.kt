23cee5b2e6d246862aa0133aa22bfa8887954972
Wanaka <jonas.haag@hotmail.com>
2021-01-12 13:28:26 +0100

    implemented splash screen

--- app/src/main/java/com/haag/accenturetest/ui/MainActivity.kt
+++ app/src/main/java/com/haag/accenturetest/ui/MainActivity.kt
@@ -11,6 +11,7 @@ import kotlinx.android.synthetic.main.main_toolbar.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
         setSupportActionBar(mainToolbar as Toolbar)
     }
